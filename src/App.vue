7333db50011571f92f819f421de20119c0f14697
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2022-10-18 12:51:22 +0200

    changes

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,12 @@
   </ul>
   <router-view></router-view>
 </template>
+
 <script setup>
 import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
-
+console.log(supabase)
 </script>
+
 <style scoped>
 </style>
