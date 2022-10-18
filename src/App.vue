08ffb09df6cb84e5aeb309cd875fed8ba40e5ade
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2022-10-18 12:01:33 +0200

    changes2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>HOLA desde vue</h1>
+  <h1>Task App</h1>
+  <ul>
+    <li><RouterLink :to="{name: 'home'}">Home</RouterLink></li>
+    <li><RouterLink :to="{name: 'login'}">Login</RouterLink></li>
+  </ul>
+  <router-view></router-view>
 </template>
 <script setup>
 </script>
